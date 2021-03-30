42b18a462de4ed428da9f5d98b191809622e2e31
Zach <78569187+ZachS2@users.noreply.github.com>
2021-03-30 09:48:26 -0400

    Add files via upload

---
+++ Homework/song.js
@@ -0,0 +1,28 @@
+//Below are attributes of one of my favortte songs, World I used to know & Comments describing what each thing does.
+
+//Title of Song
+var title = "World I Used to Know "
+var year = "| Released in July 2019 by:"
+//The artists in the Band
+var artists = {
+    1: "Scott Stapp",
+    2: "Phil X",
+    3: "Scott Stevens",
+    4: "Paul Busnell",
+    5: "Kenny Arnoff"
+}
+//The song is very good
+var songisgreat = true
+//Instruments Used
+var instruments = ["Vocals","Guitar","Backing Vocals","Bass Guitar","Drums"];
+//Genre of Music
+var genre = "Rock";
+//Length (In Seconds)
+var length = (60 * 4.08);
+
+console.log(title,year);
+console.log(artists);
+console.log(songisgreat);
+console.log(genre);
+console.log(instruments);
+console.log(length);

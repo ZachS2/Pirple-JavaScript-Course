99decf711fb8ca8203b541f1a7a483c99578cbeb
Zach <78569187+ZachS2@users.noreply.github.com>
2021-05-14 20:47:05 -0400

    Create main.js

---
+++ Homework/Homework #3 - If Statements/main.js
@@ -0,0 +1,17 @@
+//Logical Argument #1
+const allMenAreMortal = true;
+const Socrates = "aMan";
+console.log("All men are mortal.")
+console.log("Socrates is a man.")
+if (Socrates == "aMan" && allMenAreMortal==true) {
+    console.log("Therefore, socrates is  mortal")
+} 
+
+//Logical Argument #2 
+const vanillaorChocolate = true;
+const notChocolate = "not";
+console.log("This cake is either vanilla or chocolate.")
+console.log("This cake is not chocolate.")
+if (Socrates == "not" && allMenAreMortal==true) {
+    console.log("Therefore, this cake is vanilla")
+}

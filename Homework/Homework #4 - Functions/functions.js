04dd5a2123150b954030035ec55baa0188c927bd
Zach <zrsrenegade2@gmail.com>
2021-07-19 19:40:05 -0400

    Code added to functions.js

--- Homework/Homework #4 - Functions/functions.js
+++ Homework/Homework #4 - Functions/functions.js
@@ -1,1 +1,24 @@
+let men = ["Alejandro", "Socrates", "Steve"];
 
+const isMortal = (name) => {
+  let isManMortal = false;
+  men.forEach( (man) => {
+    if (man === name) {
+      isManMortal = true;
+    }
+  });
+  return isManMortal;
+};
+
+if (isMortal("Socrates")) {
+  console.log("Socrates is mortal.");
+} else {
+  console.log("Socrates is not mortal.");
+}
+
+const cake = (possibilities = [], isChocolate = false) => {
+  const option = (isChocolate === true) ? 1 : 0;
+  return "The cake is " + possibilities[option] + ".";
+}
+
+console.log(cake(["vanilla", "chocolate"], false));
